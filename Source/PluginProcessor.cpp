19b8e57ea6140e95c3a7658aec3d3a3fed776bf1
stowaway4331 <martindjeremie@gmail.com>
2022-09-06 12:06:09 +0530

    added basic FFT generator and analysis graph

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -109,6 +109,9 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
 
     
     updateFilters();
+
+    leftChannelFifo.prepare(samplesPerBlock);
+    rightChannelFifo.prepare(samplesPerBlock);
 }
 
 void SimpleEQAudioProcessor::releaseResources()
@@ -168,6 +171,9 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
 
     leftChain.process(leftContext);
     rightChain.process(rightContext);
+    
+    leftChannelFifo.update(buffer);
+    rightChannelFifo.update(buffer);
 
     /*// This is the place where you'd normally do the guts of your plugin's
     // audio processing...
